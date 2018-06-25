e4c8d569b563b0cb450868480daad8f98e484c52
Your Name <you@example.com>
2018-06-26 00:04:26 +0100

    fuse almost working

--- fuse/fuse-example.c
+++ fuse/fuse-example.c
@@ -25,6 +25,7 @@ static int getattr_callback(const char *path, struct stat *stbuf) {
 		stbuf->st_mode = S_IFDIR | 0755;
 		stbuf->st_nlink = 2;
 		return 0;
+		
 	} else {
 		stbuf->st_mode = S_IFREG | 0777;
 		stbuf->st_nlink = 1;
@@ -39,13 +40,15 @@ static int readdir_callback(const char *path, void *buf, fuse_fill_dir_t filler,
 		off_t offset, struct fuse_file_info *fi) {
 
 	char fuseLsbuf[20000];
-	int numRetVals;
+	int numRetVals = 0;
 
 	//FIXME: get cwd
-	long dirId = vfs_getIdByPath(path, 0);
-	vfs_fuseLsDir(c, dirId, fuseLsbuf, 999, &numRetVals);
+	//long dirId = vfs_getIdByPath(path, 0);
+	//vfs_fuseLsDir(c, dirId, fuseLsbuf, 999, &numRetVals);
 	int i;
 	char *ptr = fuseLsbuf;
+	filler( buf, ".", NULL, 0 );
+	filler( buf, "..", NULL, 0 );
 	for ( i = 0; i < numRetVals; i++ ) {
 		filler( buf, ptr, NULL, 0 );
 		ptr += strlen( ptr );
@@ -90,7 +93,7 @@ int main(int argc, char *argv[])
 {
         unsigned int j;
         redisReply *reply;
-        const char *hostname = (argc > 1) ? argv[1] : "127.0.0.1";
+        const char *hostname = "127.0.0.1";
         int port = (argc > 2) ? atoi(argv[2]) : 6379;
 
         struct timeval timeout = { 1, 500000 }; // 1.5 seconds
